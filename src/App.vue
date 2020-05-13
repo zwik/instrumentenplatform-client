9360c48a4a62dbdf56349d0fd1bcc5bb6ad22599
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2020-05-13 12:40:49 +0200

    Small refactor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="wrapper">
-      <div class="background"></div>
+      <div class="background" />
       <div class="content">
         <Home />
       </div>
