465e5b01e864374623998ac96f109f21d97dda2f
Marcel <m.v.d.bosch@gmail.com>
2021-02-21 19:09:57 +0100

    Revert "Temperatuur"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import { createProvider } from './vue-apollo';
-import VueApexCharts from 'vue-apexcharts';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueApexCharts);
-Vue.component('apexchart', VueApexCharts);
 
 new Vue({
   apolloProvider: createProvider(),
